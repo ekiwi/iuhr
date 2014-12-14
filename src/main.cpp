2b5bb67acc4a5739ace7dd65e8492b24f6066ba2
Electronic Kiwi <electron.kiwi@gmail.com>
2014-12-14 19:51:51 +0100

    Main: leds are glowing!!

--- src/main.cpp
+++ src/main.cpp
@@ -27,60 +27,66 @@ typedef GpioPort<GpioOutputC0, 6> RowPort;
 MAIN_FUNCTION
 {
 
+	// keep on
 	GpioOutputB7::setOutput(xpcc::Gpio::High);
+
+
+	// Trigger for Logic Analyzer
+	GpioOutputD5::setOutput(xpcc::Gpio::Low);
+	xpcc::delayMicroseconds(100);
+	GpioOutputD5::set();			// Trigger!!
+	xpcc::delayMicroseconds(100);
+
 
 	//GpioD0::connect(Uart::Rx);
 	GpioD1::connect(Uart::Tx);
-	Uart::initialize<19200>();
+	Uart::initialize<xpcc::avr::SystemClock, 19200>();
 
 	xpcc::atmega::enableInterrupts();
 	XPCC_LOG_INFO << "iUhr 0.1 says hello...." << xpcc::endl;
 
-	RowPort::write(0xfe); // turn rows off except for digit 0 by default
+	RowPort::write(~(1<<1)); // turn rows off except for digit 1 by default
 	RowPort::setOutput();
 	XPCC_LOG_DEBUG<< "RowPort initialized" << xpcc::endl;
 
 	Tlc5951::initialize();
 	XPCC_LOG_DEBUG<< "Tlc5951 initialized" << xpcc::endl;
 
-	GpioOutputD5::setOutput(xpcc::Gpio::Low);
-
 	// square wave at D5
 	//TCCR0A = (1<<COM0B1) | (1<<WGM00) | (1<<WGM01);
 	//TCCR0B = (1<<CS01) | (1<<CS00) | (1<<WGM02);
 
 
-	const uint8_t x = 0;
-	buffer[x] = 0xff;
-	buffer[x+3] = 0xff;
-	buffer[x+6] = 0xff;
-	buffer[x+9] = 0xff;
-	buffer[x+12] = 0xff;
-	buffer[x+15] = 0xff;
-	buffer[x+18] = 0xff;
-	buffer[x+21] = 0xff;
+//	const uint8_t x = 0;
+//	buffer[x] = 0xff;
+//	buffer[x+3] = 0xff;
+//	buffer[x+6] = 0xff;
+//	buffer[x+9] = 0xff;
+//	buffer[x+12] = 0xff;
+//	buffer[x+15] = 0xff;
+//	buffer[x+18] = 0xff;
+//	buffer[x+21] = 0xff;
+
+	buffer[0] = 0xff;
 
 
-
-
-
-	XPCC_LOG_DEBUG<< "Set B0 to 0xff" << xpcc::endl;
-
+	Tlc5951::writeGrayscale(buffer);
+	Tlc5951::latch();
 
 
 	uint8_t row = 0;
 	while (1)
 	{
-		++row;
-		Tlc5951::writeGrayscale(buffer);
+//		Tlc5951::writeGrayscale(buffer);
 		RowPort::write(0xff);
-		Tlc5951::latch();
+//		Tlc5951::latch();
 		RowPort::write(~(1<<row));
+		++row;
 		row = (row > 5)? 0 : row;
 
-		GpioOutputD5::toggle();
+//		GpioOutputD5::toggle();
 
-		//xpcc::delay_us(100);
+		xpcc::delayMicroseconds(100);
 	}
 
 	return 0;
